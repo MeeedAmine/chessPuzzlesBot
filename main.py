e533ff0aa6c405f256ef12ee7eb9e38376a4ae74
MeeedAmine <amine.meed@hotmail.fr>
2021-03-11 21:54:36 +0100

    added commands for the bot

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import discord
+from discord.ext import commands, tasks
 from chessdotcom import get_current_daily_puzzle, get_random_daily_puzzle
 import io
 import aiohttp
 import chess.pgn
 import json
 
+
+
 with open('token.json') as f:
     TOKEN = json.load(f)['token']
 
-client = discord.Client()
 
+bot = commands.Bot(command_prefix="!")
 
 
 def get_solution(pgn):
@@ -29,10 +32,11 @@ def get_turn(pgn):
 def daily_puzzle():
     data = get_current_daily_puzzle().json
     pgn = data['pgn']
+    turn = get_turn(pgn)
     titel = data['title']
     image_url = data['image']
     solution = get_solution(pgn)
-    return titel, image_url, solution
+    return titel, turn, image_url, solution
 
 def random_puzzle():
     data = get_random_daily_puzzle().json
@@ -43,36 +47,48 @@ def random_puzzle():
     solution = get_solution(pgn)
     return title, turn, image_url, solution
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$puzzle'):
-        global solution
-        title, turn, image_url, solution = random_puzzle()
-        await message.channel.send(f'{title}. {turn}')
-        async with aiohttp.ClientSession() as session:
+@bot.command(
+    name = 'daily_puzzle',
+    help="Type the above command to enjoy the puzzle of this day;)",
+	brief="Gives you the puzzle of today from chess.com with its solution"
+    )
+async def daillypuzzle(ctx):
+    title, turn, image_url, solution = daily_puzzle()
+    await ctx.channel.send(f'{title}. {turn}')
+    async with aiohttp.ClientSession() as session:
             async with session.get(image_url) as resp:
                 if resp.status != 200:
-                    return await message.channel.send('Could not download file...')
+                    return await ctx.channel.send('Could not download the puzzle, wait and try again!')
                 data = io.BytesIO(await resp.read())
-                await message.channel.send(file=discord.File(data, f'{title}.png'))
-                await message.channel.send(f'Solution (First, try to solve it by your self!): ||{solution}||')
-                
-    if message.content.startswith('$s'):
-        if solution:
-            await message.channel.send(f'Solution: ||{solution}||')
+                await ctx.channel.send(file=discord.File(data, f'{title}.png'))
+                await ctx.channel.send(f'Solution (First, try to solve it by your self!): ||{solution}||')
+
+@bot.command(
+    name = 'puzzle',
+    help="Type the above command to enjoy the puzzles ;)",
+	brief="Gives you a random puzzle from chess.com with its solution"
+    )
+async def puzzle(ctx):
+    title, turn, image_url, solution = random_puzzle()
+    await ctx.channel.send(f'{title}. {turn}')
+    async with aiohttp.ClientSession() as session:
+            async with session.get(image_url) as resp:
+                if resp.status != 200:
+                    return await ctx.channel.send('Could not download the puzzle, wait and try again!')
+                data = io.BytesIO(await resp.read())
+                await ctx.channel.send(file=discord.File(data, f'{title}.png'))
+                await ctx.channel.send(f'Solution (First, try to solve it by your self!): ||{solution}||')
 
 
 
 
-client.run(TOKEN)
+
+bot.run(TOKEN)
 
 
 #TODO find a way to add a frame for the coordinates of the board (svg to png!!!)
